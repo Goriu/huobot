e5f34ac66fb6f188e882585adab3c93e45496785
Ignacio_Oceja <ignacioo@softcom.com>
2018-02-01 15:13:49 +0100

    hardcoded currencies
    there are problems with 2 currencies, that retrieves empty strings, andd should retrieve a fail, becaue there are not in those makets

--- BrainBot.py
+++ BrainBot.py
@@ -4,11 +4,13 @@ from PairDictionary import *
 # import time
 
 #  currencies = ['eos','omg','ruff','zil','soc','mee','eko','link','iost','qash']
-currencies = tradingList  # List from PairDictionary.py. It contains all currencies with 2 or  3 trading pairs.
+'''currencies = tradingList  # List from PairDictionary.py. It contains all currencies with 2 or  3 trading pairs.
 # currencies = [x.encode('UTF8') for x in currencies] #  convert unicode into utf8 (normal python2.7 string)
 
 print currencies
-print "len Currencies: ",len(currencies)
+print "len Currencies: ",len(currencies)'''
+
+currencies = [u'link', u'swftc', u'dash', u'pay', u'evx', u'tnt', u'bch', u'tnb', u'theta', u'dat', u'mana', u'xrp', u'snt', u'utk', u'mco', u'ht', u'rcn', u'qun', u'wax', u'neo', u'salt', u'btm', u'eko', u'srn', u'appc', u'cmt', u'req', u'icx', u'ocn', u'zec', u'act', u'ost', u'storj', u'hsr', u'soc', u'elf', u'ven', u'gnt', u'dbc', u'trx', u'powr', u'zil', u'mee', u'nas', u'ltc', u'eos', u'iost', u'yee', u'ruff', u'rdn', u'lun', u'gnx', u'ela', u'dgd', u'eth', u'smt', u'itc', u'omg', u'stk', u'mds', u'adx', u'etc', u'aidoc', u'qtum', u'cvc', u'qsp', u'bat', u'qash', u'xem', u'gas', u'chat', u'zla', u'topc', u'propy', u'wicc']
 
 DIC = {}
 if __name__ == '__main__':
@@ -16,7 +18,7 @@ if __name__ == '__main__':
     # Get all the initial info for the crypto pairs in all the markets
     for x in currencies:
         DIC[x]= []
-        print "DIC",DIC
+        print DIC[x]
         # For BTC Market
         y = get_depth(x + 'btc','step0')
         if y['status'] == 'fail':
@@ -35,10 +37,12 @@ if __name__ == '__main__':
             DIC[x].append(y['tick']['asks'][0][0])
         # For USDT Market
         y = get_depth(x + 'usdt', 'step0')
+        print y
         if y['status'] == 'fail':
             DIC[x].append(-1)
             DIC[x].append(999999999999)
         else:
+
             DIC[x].append(y['tick']['bids'][0][0])
             DIC[x].append(y['tick']['asks'][0][0])
 
@@ -69,26 +73,32 @@ if __name__ == '__main__':
             minimum = min(DIC[x][3],DIC[x][5])
             if minimum == DIC[x][3]:
                 profit = maximum / minimum
-                print 'Compro ' + x +' en BTC Y vendo en ETH con un profit de: ', profit
+                profit1 = maximum - minimum
+                print 'Buy ' + x +' in BTC Market Y sell it in ETH with a % profit of: ', profit, " and a total profit of: ", profit1, " btc"
             elif minimum == DIC[x][5]:
                 profit = maximum / minimum
-                print 'Compro ' + x +' en BTC Y vendo en USDT con un profit de: ', profit
+                profit1 = maximum - minimum
+                print 'Buy ' + x + ' in BTC Market Y sell it in USDT with a % profit of: ', profit, " and a total profit of: ", profit1, " btc"
         if maximum == DIC[x][2]:
             minimum = min(DIC[x][1],DIC[x][5])
             if minimum == DIC[x][1]:
                 profit = maximum / minimum
-                print 'Compro ' + x +' en ETH Y vendo en BTC con un profit de: ', profit
+                profit1 = maximum - minimum
+                print 'Buy ' + x + ' in ETH Market Y sell it in BTC with a % profit of: ', profit, " and a total profit of: ", profit1, " btc"
             elif minimum == DIC[x][5]:
                 profit = maximum / minimum
-                print 'Compro ' + x +' en ETH Y vendo en USDT con un profit de: ', profit
+                profit1 = maximum - minimum
+                print 'Buy ' + x + ' in ETH Market Y sell it in USDT with a % profit of: ', profit, " and a total profit of: ", profit1, " btc"
         if maximum == DIC[x][4]:
             minimum = min(DIC[x][1],DIC[x][3])
             if minimum == DIC[x][1]:
                 profit = maximum / minimum
-                print 'Compro ' + x +' en USDT Y vendo en BTC con un profit de: ', profit
-            elif minimum == DIC[x][5]:
+                profit1 = maximum - minimum
+                print 'Buy ' + x + ' in USDT Market Y sell it in BTC with a % profit of: ', profit, " and a total profit of: ", profit1, " btc"
+            elif minimum == DIC[x][3]:
                 profit = maximum / minimum
-                print 'Compro ' + x +' en USDT Y vendo en ETH con un profit de: ', profit
+                profit1 = maximum - minimum
+                print 'Buy ' + x + ' in USDT Market Y sell it in ETH with a % profit of: ', profit, " and a total profit of: ", profit1, " btc"
 
 
 
